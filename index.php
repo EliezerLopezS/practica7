4a72fc3df9da1b8bbeb03f97ddeea18ceb2b9df7
Eliezer Lopez Sandoval <371EZE6.375@GMAIL.COM>
2020-04-30 20:06:47 -0500

    sds

--- index.php
+++ index.php
@@ -6,9 +6,17 @@
   </head>  
   <body>
     <a-scene embedded arjs>
-      <a-entity scale=".2 .2 .2"> 
+      <!--a-entity scale=".2 .2 .2"> 
         <a-entity gltf-model="model.gltf" scale="0.8 0.8 0.8" crossOrigin="anonymous">
-        </a-entity>
+        </a-entity-->
+      <a-assets>
+        <a-asset-item id="obj" src="obje.obj"></a-asset-item>
+      </a-assets>
+
+      <a-entity obj-model="obj: #obj" position="1 -1 -30">
+        <a-animation attribute="rotation" dur="12000" easing="linear" repeat="indefinite"
+                     to="0 360 0"></a-animation>
+      </a-entity>
         <a-marker-camera preset='hiro'></a-marker-camera>
         </a-scene>
       </body>
